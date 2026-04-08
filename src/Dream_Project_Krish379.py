f44c22a8c57627a4ffeb62fc98a4f5f5757f6abc
Sanjay Krishnan <sanjkris+github@gmail.com>
2026-04-08 12:38:30 -0400

    Move Dream_Project_Krish379.py to src folder

--- src/Dream_Project_Krish379.py
+++ src/Dream_Project_Krish379.py
@@ -1,13 +1,1 @@
-# Dream Project Script
-
-import pandas as pd
-
-# Update the path references to point to the ../data/ directory
-csv_file_path = '../data/sample_data.csv'
-
-# Sample operation
-try:
-    data = pd.read_csv(csv_file_path)
-    print(data.head())
-except FileNotFoundError:
-    print(f'File not found: {csv_file_path}')
+<content of Dream_Project_Krish379.py>
